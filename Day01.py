b20e40872818ac9ae639f49638974d499e96de11
Shivani Shinde <31477382+erIamshivani@users.noreply.github.com>
2019-01-18 17:46:58 +0530

    Create Day01.py

---
+++ Day01.py
@@ -0,0 +1,31 @@
+#Task:
+#sum of no.s from 0 to 100
+n=100
+sum= (n * (n+1)) / 2
+print(sum)
+
+#-------------------------------------
+#Day1 Activity
+print((5 > 4) and (3 == 5)) #false
+print(not (5 > 4)) #false
+print((5 > 4) or (3 == 5)) #true
+print(not ((5 > 4) or (3 == 5))) #false
+print((True and True) and (True == False)) #false
+print((not False) or (not True)) #true
+print("----------------------------")
+
+#If else code
+spam=1
+if spam==1:
+print("Hello")
+elif spam==2:
+print("Howdy")
+else:
+print("Greetings!")
+print("----------------------------")
+
+print(round(4.3)) #prints 4
+print(round(4.64)) #prints 5
+print(abs(-555)) #prints 555
+print(abs(3-4j)) #prints 5
+#-------------------------------------------
